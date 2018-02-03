4c9e8d49244b476b0bd21d3032d8de069b1ee165
Chris Hakos <chrishakos@gmail.com>
2018-02-02 19:23:51 -0500

    add slug logic

--- typecodeapp/app/models/article.rb
+++ typecodeapp/app/models/article.rb
@@ -1,4 +1,25 @@
 class Article < ActiveRecord::Base
   validates :title, :slug, presence: true
   validates :slug, uniqueness: true
+
+  attr_accesible :name, :slug
+
+  def to_param
+    slug
+  end
+
+  def validate_slug(slug)
+    if Article.find_by_slug(slug)
+      return true
+    else
+      return false
+    end
+  end
+
+  def append_to_slug(slug)
+    random_string = ""
+    return slug + "-" + random_string
+  end
+
+
 end
